5b9b935266bcc50fb287b3e3f47c57f4786b092c
shahinalom <shahinalom3511371@gmail.com>
2025-03-15 03:31:13 +0600

    heading updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Container from '../components/Container'
 import '../components/banner.css'
 import img from '../assets/girl.webp'
+import { FaArrowRightLong } from 'react-icons/fa6';
 const Banner = () => {
       return (
             <div>
                   <Container className={"banner-bg-img"}>
-                        <img src={img} alt="img" className='lg:h-[600px] pt-16 lg:ml-[600px]' />
+                        <img src={img} alt="img" className='lg:h-[600px] pt-16 lg:ml-[750px] relative' />
+                        <div className='absolute ml-28 top-48'>
+                              <div className='font-semibold text-gray-800  text-5xl uppercase  space-y-4' >
+                                    <h1>Hero-Shape-1 Get<span className='text-[#ee4a62] font-bold'> 2500+</span></h1>
+                                    <h1>Best Online Courses</h1>
+                                    <h1>From EduBlink</h1>
+                              </div>
+                              <p className='text-xl text-gray-800 mt-6'>Excepteur sint occaecat cupidatat non proident sunt in culpa <br /> qui officia deserunt mollit.
+                              </p>
+                              <button className='flex items-center gap-2 bg-[#23b792] py-2 px-4 text-white mt-6'>Find Course <FaArrowRightLong /></button>
+                        </div>
                   </Container>
             </div>
       );
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Container from './Container';
 import logo from '../assets/logo.png'
+import { LuLogIn } from 'react-icons/lu';
+import { IoClose } from 'react-icons/io5';
 const Heading = () => {
       const nav = [
             { title: "Home", path: "/" },
@@ -12,25 +14,36 @@ const Heading = () => {
       ]
       return (
             <div>
-                  <Container >
-                        <div className={"flex items-center justify-between"}>
-                              <img src={logo} alt="logoimg" />
-                              <div className='flex items-center'>
-                                    {
-                                          nav.map((item, index) => (
-                                                <div key={index}>
-                                                      <Link to={item?.path}>
-                                                            {item?.title}
-                                                      </Link>
-                                                </div>
-                                          ))
-                                    }
-                              </div>
-                              <div>
-                                    <button>Login</button>
-                              </div>
+                  <div className={"flex items-center justify-between  py-4 px-5 lg:max-w-screen-xl mx-auto"}>
+                        {/* bg-gradient-to-r from-[#24cef0] to-[#60e2d8] */}
+                        {/* Logo */}
+                        <div>
+                              <Link to={"/"}>
+                                    <img src={logo} alt="logo" />
+                              </Link>
                         </div>
-                  </Container>
+                        {/* Nav item */}
+                        <div className='lg:flex items-center gap-x-10 hidden md:block'>
+                              {
+                                    nav.map((item, index) => (
+                                          <div key={index}>
+                                                <Link className='text-[18px]' to={item?.path}>
+                                                      {item?.title}
+                                                </Link>
+                                          </div>
+                                    ))
+                              }
+                        </div>
+                        {/* Login Button */}
+                        <div className='hidden md:block'>
+                              <button className='bg-[#23b792] py-2 px-4 flex items-center gap-1 text-[18px] text-white'>Login <LuLogIn />
+                              </button>
+                        </div>
+                        {/* Mobile Menu */}
+                        <div className='md:hidden block'>
+                              <IoClose size={30} />
+                        </div>
+                  </div>
             </div>
       );
 };
